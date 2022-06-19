dee2cc5a286f35f4c8c5f1401b1c23d957414532
john.njau <john.njoroge@student.moringaschool.com>
2022-06-19 22:54:12 +0300

    feature: Added the business and amenities views

--- app/views.py
+++ app/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 
+from django.urls import reverse
+
+from django.db.models import Q
+from itertools import chain
+
 from django.contrib.auth.decorators import login_required
 
-from .models import Neighborhood, User, Profile, Business, Post
+from .models import Neighborhood, User, Profile, Business, Post, SocialAmenities
 from .forms import UpdateProfileForm, SignupForm, PostForm, UpdateUserForm
 
 # Create your views here.
@@ -13,21 +18,24 @@ def index(request):
 
 
 @login_required(login_url='/accounts/login/')
-def home(request):
+def home(request, neighborhood_id):
+    neighborhood = get_object_or_404(Neighborhood, pk=neighborhood_id)
     posts = Post.objects.all()
     if request.method == 'POST':
-        form = PostForm(request.POST)
+        form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
             post.user = request.user
             post.save()
+            return redirect('home', neighborhood.id)
             
     else:
         form = PostForm()
         
     params = {
         'posts': posts,
-        'form': form
+        'form': form,
+        'neighborhood': neighborhood,
     }
     return render(request, 'main/home.html', params)
 
@@ -45,12 +53,37 @@ def profile(request, username):
 
 
 @login_required(login_url='/accounts/login/')
+def user_profile(request, username):
+    user = get_object_or_404(User, username=username)
+    
+    context = {
+        'user': user,
+    }
+
+    return render(request, 'profile/other_user.html', context)
+
+@login_required(login_url='/accounts/login/')
 def search_results(request):
     if 'query' in request.GET and request.GET['query']:
         search_term = request.GET.get('query')
-        searched_query = Neighborhood.find_neighborhood(search_term)
-        message = f"{search_term}"
-        return render(request, 'search/search.html', {"message": message, "neighborhoods": searched_query})
+        # searched_query = Neighborhood.find_neighborhood(search_term)
+        searched_business = Business.objects.filter(name__icontains=search_term)
+        searched_post = Post.objects.filter(post__icontains=search_term)
+        searched_social = SocialAmenities.objects.filter(name__icontains=search_term)
+        
+        message = f"{search_term, searched_business, searched_post, searched_social}"
+        
+        results = chain(searched_business, searched_post,searched_social)
+        
+        params = {
+            'message': message,
+            'results': results,
+            'businesses': searched_business,
+            'posts': searched_post,
+            'social_amenities': searched_social
+        }
+        
+        return render(request, 'search/search.html', params)
     else:
         message = "You haven't searched for any term"
         return render(request, 'search/search.html', {"message": message})
@@ -93,10 +126,23 @@ def neighborhood(request):
 
 
 @login_required(login_url='/accounts/login/')
-def businesses(request):
-    businesses = Business.objects.all()
+def businesses(request, neighborhood_id):
+    neighborhood = get_object_or_404(Neighborhood, pk=neighborhood_id)
+    businesses = Business.objects.filter(neighborhood=neighborhood)
     
     params = {
         'businesses': businesses
     }
     return render(request, 'main/businesses.html', params)
+
+
+def social_amenities(request, neighborhood_id):
+    neighborhood = get_object_or_404(Neighborhood, pk=neighborhood_id)
+    amenities=SocialAmenities.objects.filter(neighborhood=neighborhood)
+    
+    params = {
+        'amenities': amenities
+    }
+    return render(request, 'main/social_amenities.html', params)
+
+
