e47b67e35055986b0aa7cb6cb1e88adb36a5dcf8
john.njau <john.njoroge@student.moringaschool.com>
2022-06-19 22:56:13 +0300

    feature: Added amenities urls

--- app/urls.py
+++ app/urls.py
@@ -4,12 +4,13 @@ from . import views
 
 urlpatterns =[
     path('', views.index, name='index'),
-    path('home/', views.home, name='home'),
+    path('home/<int:neighborhood_id>/', views.home, name='home'),
     path('search/',views.search_results,name='search'),
     path('profile/<str:username>/', views.profile, name='profile'),
+    path('other/profile/<str:username>/', views.user_profile, name='user_profile'),
     path('update/profile/<str:username>/', views.updateprofile, name='updateprofile'),
     path('neighborhoods/', views.neighborhood, name='neighborhood'),
-    path('businesses/', views.businesses, name='businesses'),
-
+    path('businesses/<int:neighborhood_id>/', views.businesses, name='businesses'),
+    path('social_amenities/<int:neighborhood_id>/', views.social_amenities, name='social_amenities'),
     
 ]
